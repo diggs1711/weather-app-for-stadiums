9549cfa723651bd8ba023c0672ccb77e200749e5
Higgins <dahiggins@deloitte.ie>
2017-03-28 15:20:12 +0100

    m

--- project/static/scripts/js/bundle.js
+++ project/static/scripts/js/bundle.js
@@ -1,1 +1,229 @@
-!function(t){function n(i){if(e[i])return e[i].exports;var s=e[i]={i:i,l:!1,exports:{}};return t[i].call(s.exports,s,s.exports,n),s.l=!0,s.exports}var e={};return n.m=t,n.c=e,n.i=function(t){return t},n.d=function(t,e,i){n.o(t,e)||Object.defineProperty(t,e,{configurable:!1,enumerable:!0,get:i})},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,n){return Object.prototype.hasOwnProperty.call(t,n)},n.p="",n(n.s=2)}([function(t,n){!function(){var n={events:[],publish:function(t,n){this.events.map(function(e){e.eve===t&&e.fn.call(e.scope,n)})},subscribe:function(t,n,e){this.events.push({eve:t,fn:n,scope:e})}};t.exports=n}()},function(t,n,e){!function(){var n=e(0),i={pubSub:n,init:function(){},stadiums:{},getStadiums:function(){return this.stadiums},fetchStadiums:function(){n.publish("loadingStadiums",""),$.ajax({url:"stadiums.json",dataType:"json",success:function(t){i=t,n.publish("stadiumsLoaded",t)}.bind(this),error:function(t,n,e){console.log("An error ("+n+") occured:",e.toString())}.bind(this)})}};t.exports=i}()},function(t,n,e){!function(){var t=e(0),n=e(1),i={pubSub:t,stadiums:n,stadiumList:null,loadingEle:null,run:function(){this.init(),this.stadiums.fetchStadiums()},init:function(){this.initEle()},initEle:function(){this.stadiumList=document.querySelector(".stadium-list"),this.loadingEle=document.querySelector(".loading")},handleChange:function(t){},render:function(t){var n=this,e=t.stadiums;this.loadingEle.classList.add("hidden"),e.map(function(t){var e=document.createElement("li");e.className="list-group-item";var i=n.createWeatherInfoElement(t);e.appendChild(i),this.stadium-n.stadiumList.appendChild(e)})},createWeatherInfoElement:function(t){var n=document.createElement("div");n.className="stadium-list__item";for(var e in t)if(t.hasOwnProperty(e)){var i=document.createElement("p");i.innerText=e+": "+t[e],n.appendChild(i)}return n},loadingElement:function(){this.loadingEle.classList.remove("hidden")}};t.subscribe("stadiumsLoaded",i.render,i),t.subscribe("loadingStadiums",i.loadingElement,i),i.run()}()}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+;(function() {
+  var pubSub = {
+    events: [],
+
+    publish: function(eve, data) {
+        this.events.map(function(e) {
+          if(e.eve === eve) {
+            e.fn.call(e.scope, data);
+          }
+        })
+    },
+
+    subscribe: function(eve, fn, scope) {
+
+        this.events.push({
+          eve: eve,
+          fn: fn,
+          scope: scope
+        });
+
+    }
+  };
+
+  module.exports = pubSub;
+})();
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+;(function() {
+        var pubSub = __webpack_require__(0);
+
+        var stadiums = {
+            pubSub: pubSub,
+            init: function() {
+
+            },
+
+            stadiums: {
+
+            },
+
+            getStadiums: function() {
+                return this.stadiums;
+            },
+
+            fetchStadiums: function() {
+                pubSub.publish("loadingStadiums", "");
+                
+                $.ajax({
+                    url: 'stadiums.json',
+                    dataType: 'json',
+                    success: function(data) {
+                        stadiums = data;
+                        pubSub.publish("stadiumsLoaded", data);
+                    }.bind(this),
+                    error: function(xhr, status, error) {
+                        console.log('An error (' + status + ') occured:', error.toString());
+                    }.bind(this)
+                });
+
+            },
+        };
+
+        module.exports = stadiums;
+})();
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+;
+(function() {
+    var pubSub = __webpack_require__(0);
+    var stadiums = __webpack_require__(1);
+
+    var app = {
+        pubSub: pubSub,
+        stadiums: stadiums,
+        stadiumList: null,
+        loadingEle: null,
+
+        run: function() {
+            this.init();
+            this.stadiums.fetchStadiums();
+        },
+        // sets initial state
+        init: function() {
+            this.initEle();
+        },
+
+        initEle: function() {
+            this.stadiumList = document.querySelector('.stadium-list');
+            this.loadingEle = document.querySelector('.loading');
+        },
+
+        // sets state, triggers render method
+        handleChange: function(event) {
+
+        },
+
+        render: function(data) {
+            var self = this;
+            var stadiums = data["stadiums"];
+            this.loadingEle.classList.add("hidden");
+            /*var searchString = this.state.searchString.trim().toLowerCase();
+
+            // filter countries list by value from input boxz
+            if (searchString.length > 0) {
+                stadiums = stadiums.filter(function(stadium) {
+                    return stadium.city.toLowerCase().match(searchString);
+                });
+            }
+*/
+            stadiums.map(function(stadium) {
+                var e = document.createElement("li");
+                e.className = "list-group-item"
+                var r = self.createWeatherInfoElement(stadium);
+                e.appendChild(r);
+                this.stadium - self.stadiumList.appendChild(e);
+            });
+
+        },
+
+        createWeatherInfoElement: function(s) {
+            var result = document.createElement("div");
+            result.className = "stadium-list__item";
+
+            for (var property in s) {
+                if (s.hasOwnProperty(property)) {
+                    var p = document.createElement("p");
+                    p.innerText = property + ": " + s[property];
+                    result.appendChild(p);
+                }
+            }
+
+            return result;
+        },
+
+        loadingElement: function() {
+
+            this.loadingEle.classList.remove("hidden");
+        }
+
+    };
+
+    pubSub.subscribe("stadiumsLoaded", app.render, app);
+    pubSub.subscribe("loadingStadiums", app.loadingElement, app);
+    app.run();
+})();
+
+
+/***/ })
+/******/ ]);
